b9bd473d34657c65b7767eca952f754a1c7b6989
Leomaris Reyes <reyes.leomaris@gmail.com>
2021-04-27 20:00:12 -0400

    Replicating Book Worm UI in Xamarin Forms

---
+++ BookWormUISample/Views/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace BookWormUISample.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ BookWormUISample/Views/RoomPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace BookWormUISample.Views
+{
+    public partial class RoomPage : ContentPage
+    {
+        public RoomPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
